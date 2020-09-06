88060eb1c320f7282c8ba6957c24678a1d188063
Livia Boeriu <boeriulivia@gmail.com>
2020-09-06 20:10:48 +0200

    Created .gitignore file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+
+
+
+
 
 app.listen(5000, () => {
     console.log("server has started");
