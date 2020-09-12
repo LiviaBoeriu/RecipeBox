3e953a210b86ea5b25a7ba22e149d6908e197531
Livia Boeriu <boeriulivia@gmail.com>
2020-09-12 18:05:07 +0200

    Modified adding recipe router

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -6,7 +6,6 @@ const authorization = require("../middleware/authorization");
 // Get recipe info
 router.get("/recipes", authorization, async (req, res) => {
     try {
-        // console.log(req.query);
         // Destructure req.body
         const { userId } = req;
 
@@ -28,16 +27,59 @@ router.post("/recipes", authorization, async (req, res) => {
 
         // Destructure req
         const { userId } = req;
-        const { recipeName, description } = req.body;
+        const { recipeName, description, ingredients, instructions } = req.body;
+
+        console.log(ingredients);
+
+        // Map all values in ingredients and instructions
+        let ingredientName = ingredients.map(({ ingredientName }) => ingredientName);
+        let measuringUnit = ingredients.map(({ measuringUnit }) => measuringUnit);
+        let amount = ingredients.map(({ amount }) => amount);
+        let instructionNumber = instructions.map(({ instructionNumber }) => instructionNumber);
+        let instructionText = instructions.map(({ instructionText }) => instructionText);
+
+        // Get container id
         const containerQuery = await pool.query("SELECT id FROM recipe_container WHERE userid = $1", [userId]);
-        const containerid = containerQuery.rows[0].id;
-
-
-        // Need to add validation to check if the recipe exists already
+        const containerId = containerQuery.rows[0].id;
 
         // Add recipe
-        const newRecipe = await pool.query("INSERT INTO recipe (name, containerid, description, userid) VALUES ($1, $2, $3, $4) RETURNING *", [recipeName, containerid, description, userId]);
+        const newRecipe = await pool.query("INSERT INTO recipe (name, containerid, description, userid) VALUES ($1, $2, $3, $4) RETURNING *", [recipeName, containerId, description, userId]);
 
+        // Add ingredients to ingredient table
+        // get recipe id
+        const recipeId = newRecipe.rows[0].id;
+        console.log(recipeId);
+
+        // loop through all ingredients 
+        for(let ingredient in ingredients) {
+
+            // check if the ingredient exists if not add it to the table
+            const ingredientInDb = await pool.query("SELECT * FROM ingredient WHERE ingredientname LIKE $1", [ingredientName[ingredient]]);
+
+            // if(ingredientInDb.rows[0]){
+            //     res.json(ingredientInDb.rows[0]);
+            // }
+
+            // Add ingredients, recipeId to recipe_ingredients table
+            // loop through ingredients and add them to the recipe ingredient with their respective recipe id and amount
+            const newIngredient = await pool.query("INSERT INTO ingredient (ingredientname, measuringunit) VALUES ($1, $2) RETURNING *", [ingredientName[ingredient], measuringUnit[ingredient]]);
+
+            // Get ingredient ids and add them to the next recipe
+            const ingredientId = newIngredient.rows[0].id;
+
+            const newRecipeIngredient = await pool.query("INSERT INTO recipe_ingredient (recipeid, ingredientid, amount) VALUES ($1, $2, $3) RETURNING *", [recipeId, ingredientId, amount[ingredient]]);
+
+        }
+
+        for(instruction in instructions) {
+
+            // Add instructions to instructions table with recipeId
+            // Loop through all instructions and add them to the table along with their number and recipeId
+            const newInstruction = await pool.query("INSERT INTO instructions (instruction_number, instruction_text, containerid, recipeid) VALUES ($1, $2, $3, $4) RETURNING *", [instructionNumber[instruction], instructionText[instruction], containerId, recipeId]);
+
+        }
+
+        // return an object containing all fields
         res.json(newRecipe.rows[0]);
 
     } catch (err) {
@@ -46,63 +88,6 @@ router.post("/recipes", authorization, async (req, res) => {
     }
 });
 
-
-// Add new recipe instructions
-router.post("/recipes/instructions", authorization, async (req, res) => {
-    try {
-
-        // Destructure req
-        const { userId } = req;
-        const { recipeid, instructionNumber, instructionText } = req.body;
-        const containerQuery = await pool.query("SELECT id FROM recipe_container WHERE userid = $1", [userId]);
-        const containerid = containerQuery.rows[0].id;
-        let newInstruction;
-        const recipe = await pool.query("SELECT * FROM recipe WHERE id = $1", [recipeid]);
-
-        // Verify if the recipe exists
-        if(recipe && !recipe.rows[0]) {
-            res.status(404).send("Not found");    
-        }
-        
-        const instruction = await pool.query("SELECT * FROM instructions WHERE instruction_number=$1 AND recipeid=$2", [instructionNumber, recipeid]);
-        console.log(instruction.rows[0]);
-        // If instruction does not exist insert it, else return error message
-        if(instruction && !instruction.rows[0]) {
-            newInstruction = await pool.query("INSERT INTO instructions (instruction_number, instruction_text, containerid, recipeid) VALUES ($1, $2, $3, $4) RETURNING *", [instructionNumber, instructionText, containerid, recipeid]);
-            res.json(newInstruction.rows[0]);
-        } else {
-            res.status(403).send("Instruction already exists");
-        }
-    } catch (err) {
-        console.error(err.message);
-        res.send("Server Error");
-    }
-});
-
-// Edit instruction
-router.patch("/recipes/:recipeId/instructions/:instructionId", authorization, async (req, res) => {
-    try {
-
-        const { instructionText } = req.body;
-        const { recipeId, instructionId} = req.params;
-
-        const recipe = await pool.query("SELECT * FROM recipe WHERE id = $1", [recipeId]);
-        console.log(recipe.rows);
-        // Verify if the recipe exists
-        if(recipe && !recipe.rows[0]) {
-            res.status(404).send("Not found");    
-        }
-
-        // Update recipe
-        const updatedInstruction = await pool.query("UPDATE instructions SET instruction_text=($1) WHERE id = $2 RETURNING *", [instructionText, instructionId]);
-
-        res.json(updatedInstruction);
-
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-    }
-});
 
 
 // Edit recipe
@@ -178,5 +163,7 @@ router.get("/recipes/search", authorization, async (req, res) => {
         res.status(500).send("Server Error");
     }
 });
+
+
 
 module.exports = router;
